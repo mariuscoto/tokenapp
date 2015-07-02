3a0ceb061030392571513da82339450e6ae8294b
Marius Cotofana <marius.cotofana@hootsuite.com>
2015-07-02 10:58:50 +0300

    Add bg image and pub img

--- generator.js
+++ generator.js
@@ -10,6 +10,7 @@ var app = module.exports = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
